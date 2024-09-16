403b36ee7b13c646ef6b89f1adb911ecc4026e1e
cagataysengor <sengorcagatay@gmail.com>
2024-09-16 16:37:52 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Dersler sayfasına yönlendirme fonksiyonu
+function dersSayfasinaGit() {
+    window.location.href = "dersler.html"; // "Derslere Başla" butonu ile dersler sayfasına yönlendirilir
+}
+
+// Quiz sayfasına yönlendirme fonksiyonu
+function quizSayfasinaGit() {
+    window.location.href = "quiz.html"; // "Quiz'e Katıl" butonu ile quiz sayfasına yönlendirilir
+}
